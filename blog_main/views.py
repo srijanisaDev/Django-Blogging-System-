e8a70bf4986dba4fb1a065d809343f48f2ff984c
srijanisaDev <srijankumar770@gmail.com>
2026-05-08 08:55:31 +0530

    configured the home page rendering posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blogs.models import Category , Blog
 
 
 
 def home(request):
-    return render(request , 'home.html')
+    categories = Category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True).order_by('updated_at')
+    context = {
+        'categories' : categories,
+        'featured_posts' : featured_post,
+    }
+    return render(request , 'home.html' , context)
