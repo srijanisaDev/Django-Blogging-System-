3555685b4e4e60d35ce6208ffcc7ffc1067469f1
srijanisaDev <srijankumar770@gmail.com>
2026-05-09 22:48:19 +0530

    posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -6,9 +6,12 @@ from blogs.models import Category , Blog
 
 def home(request):
     categories = Category.objects.all()
-    featured_post = Blog.objects.filter(is_featured=True).order_by('updated_at')
+    featured_post = Blog.objects.filter(is_featured=True , status = 'Published').order_by('updated_at')
+    posts = Blog.objects.filter(is_featured = False , status = 'Published')
+    
     context = {
         'categories' : categories,
         'featured_posts' : featured_post,
+        'posts' : posts
     }
     return render(request , 'home.html' , context)
