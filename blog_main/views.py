b8830516ed94299665bc9d2194d9096fa4ffc4cb
srijanisaDev <srijankumar770@gmail.com>
2026-05-14 22:00:34 +0530

    added dynamic about us

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Category , Blog
+from assignments.models import About
 
 
 
@@ -8,10 +9,19 @@ def home(request):
     # categories = Category.objects.all()
     featured_post = Blog.objects.filter(is_featured=True , status = 'Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured = False , status = 'Published')
-    
+
+    ## Fetch ABout us 
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
+
+
     context = {
         # 'categories' : categories,
         'featured_posts' : featured_post,
-        'posts' : posts
+        'posts' : posts,
+        'about' : about,
     }
     return render(request , 'home.html' , context)
