f187139b7eddf137a01ac201f5f8703aac937c18
srijanisaDev <srijankumar770@gmail.com>
2026-05-17 10:12:07 +0530

    added registration , login , logout functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     #Search endpoint
     path('blogs/search/', BlogsView.search , name='search'),
     path('register/',views.register , name = 'register'),
+    path('login/' , views.login , name="login"),
+    path('logout/' , views.logout , name='logout'),
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render , redirect
 from blogs.models import Category , Blog
 from assignments.models import About
 from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 
@@ -43,3 +45,27 @@ def register(request):
         'form': form,
     }
     return render(request , 'register.html' , context)
+
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username , password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')    
+    form = AuthenticationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request,'login.html',context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
