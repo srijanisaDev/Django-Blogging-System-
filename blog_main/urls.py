11bec6b7f6963e769140c567d20840dffbac7079
srijanisaDev <srijankumar770@gmail.com>
2026-05-18 22:18:22 +0530

    initiated dashboard creation for managers and editors

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -27,9 +27,13 @@ urlpatterns = [
     path("",views.home , name = 'home'),
     path('category/' , include('blogs.urls')),
     path('blogs/<slug:slug>/',BlogsView.blogs , name='blogs'),
+
     #Search endpoint
     path('blogs/search/', BlogsView.search , name='search'),
     path('register/',views.register , name = 'register'),
     path('login/' , views.login , name="login"),
     path('logout/' , views.logout , name='logout'),
+
+    #Dashboards
+    path('dashboards/',include('dashboards.urls'))
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
